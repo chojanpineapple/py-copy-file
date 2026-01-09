aa1acd807a22b7b2f2b6109940e7d23ca6aaac71
Johan Rangel <johan.apr99@gmail.com>
2026-01-09 17:04:34 +0000

    new test

--- app/main.py
+++ app/main.py
@@ -1,28 +1,17 @@
-import os
-
-
 def copy_file(command: str) -> None:
     parts = command.split()
-    validador = True
 
     if len(parts) != 3 or parts[0] != "cp":
-        validador = False
         return
 
-    source = parts[1]
-    target = parts[2]
+    _, source_file, target_file = parts
 
-    if source == target:
-        validador = False
+    if source_file == target_file:
         return
 
-    if not os.path.isfile(source):
-        validador = False
+    try:
+        with (open(source_file, "r") as
+              file_in, open(target_file, "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
         return
-    
-    if validador:
-        with open(source, "r") as src:
-            content = src.read()
-
-        with open(target, "w") as dst:
-            dst.write(content)

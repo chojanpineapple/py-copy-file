3495510d05ac20060771d8fee9e27bbf3bd8e483
chojanpineapple <johan.apr99@gmail.com>
2026-01-12 12:25:18 +0000

    new test1

--- app/main.py
+++ app/main.py
@@ -16,8 +16,6 @@ def copy_file(command: str) -> None:
     if not os.path.isfile(source):
         return
 
-    with open(source, "r") as src:
+    with open(source, "r") as src, open(target, "x") as dst:
         content = src.read()
-
-    with open(target, "w") as dst:
         dst.write(content)
